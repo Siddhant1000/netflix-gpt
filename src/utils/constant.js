aab99a15a6deca039e0c59eb1ec3be61d623f2a8
Siddhant <sidvaidwal73@gmail.com>
2024-05-29 03:56:34 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,17 +1,1 @@
-export const NETFIX_LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/2560px-Netflix_logo.svg.png";
-
-export const PHOTO_AVATAR =  "https://i.pinimg.com/474x/5b/50/e7/5b50e75d07c726d36f397f6359098f58.jpg";
-
-
-export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
-
-export const BACKGROUND_IMG = "https://miro.medium.com/v2/resize:fit:1400/1*5lyavS59mazOFnb55Z6znQ.png?w=1920&h=1080";
-
-
-export const SUPPORTED_LANGUAGES = [
-  {identifier : "en", name:"English" },
-{identifier : "hindi", name:"Hindi" },
-{identifier : "spanish", name:"Spanish" }
-]
-
 
